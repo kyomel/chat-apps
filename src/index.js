c51c76c7447b4fbd62b91d15c9c1779ee0409f07
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-05-31 14:01:02 +0700

    [kyomel] chat apps realtime using socket.io

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
+
+io.on('connection', (socket) => {
+    console.log('New Websocket connection');
+    socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined!');
+    socket.on('sendMessage', (message, callback) => {
+        io.emit('message', message);
+        callback('Delivered!');
+    })
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    })
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left')
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`);
+})
