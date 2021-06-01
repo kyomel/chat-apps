4051d98184fa8c8577ae1cae91cbdcaacd5fe5af
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-06-01 10:02:51 +0700

    [kyomel] styling app chat is done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 
 const app = express();
 const server = http.createServer(app);
@@ -14,17 +16,22 @@ app.use(express.static(publicDirectoryPath));
 
 io.on('connection', (socket) => {
     console.log('New Websocket connection');
-    socket.emit('message', 'Welcome!')
-    socket.broadcast.emit('message', 'A new user has joined!');
+    socket.emit('message', generateMessage('Welcome!'));
+    socket.broadcast.emit('message', generateMessage('A new user has joined!'));
     socket.on('sendMessage', (message, callback) => {
-        io.emit('message', message);
-        callback('Delivered!');
+        const filter = new Filter();
+        if(filter.isProfane(message)) {
+            return callback('Profanity is not allowed');
+        }
+        io.emit('message', generateMessage(message));
+        callback();
     })
-    socket.on('sendLocation', (coords) => {
-        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`));
+        callback();
     })
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left')
+        io.emit('message', generateMessage('A user has left'))
     })
 })
 
